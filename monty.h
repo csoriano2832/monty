ecde6fca470f85769a315387065aea6222726d6e
Thegermanblob <thegermanblob@gmail.com>
2021-05-12 02:33:03 +0000

    Add monty

--- monty.h
+++ monty.h
@@ -2,6 +2,7 @@
 #define MONTY_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 
 /**
@@ -33,5 +34,10 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+void push (stack_t **stack, unsigned int line_number);
+void pall (stack_t **stack, unsigned int line_number);
+
+int errors(int err, char *str);
 
 #endif
