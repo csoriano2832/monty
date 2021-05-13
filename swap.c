d34292704027f5ae53c4f50cbb77f4b31b15b559
csoriano2832 <2832@holbertonschool.com>
2021-05-13 20:18:59 +0000

    Print errors on stderr

--- swap.c
+++ swap.c
@@ -19,7 +19,7 @@ void swap(stack_t **stack, unsigned int line_number)
 
 	if (elements < 2)
 	{
-		printf("L%u: can't swap, stack too short", line_number);
+		fprintf(stderr, "L%u: can't swap, stack too short", line_number);
 		exit(EXIT_FAILURE);
 	}
 
